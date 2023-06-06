f1b546fb89219b13138ee1454e55f1b639a4faa4
XueyangSong <jamiesxy09@gmail.com>
2023-06-06 04:41:30 -0700

    add end of game scroller

---
+++ Assets/ImageScroller.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageScroller : MonoBehaviour
+{
+    float speed = 20.0f;
+    // float imagePosBegin = -1550.0f;
+    float boundaryTextEnd = 530.0f;
+
+    RectTransform myGorectTransform;
+
+    // [SerializeField]
+    // Image image;
+
+    // [SerializeField]
+    // bool isLooping = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myGorectTransform = gameObject.GetComponent<RectTransform>();
+        StartCoroutine(AutoScrollText());
+    }
+
+    IEnumerator AutoScrollText()
+    {
+        while(myGorectTransform.localPosition.y < boundaryTextEnd) {
+            myGorectTransform.Translate(Vector3.up * speed * Time.deltaTime);
+            yield return null;
+        }
+    }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+}
---
+++ Assets/Scripts/AutoScroll.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class AutoScroll : MonoBehaviour
+{
+    float speed = 20.0f;
+    // float textPosBegin = -992.0f;
+    float boundaryTextEnd = 695.0f;
+
+    RectTransform myGorectTransform;
+    // [SerializeField]
+    // TextMeshProUGUI mainText;
+
+    // [SerializeField]
+    // bool isLooping = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myGorectTransform = gameObject.GetComponent<RectTransform>();
+        StartCoroutine(AutoScrollText());
+    }
+
+    IEnumerator AutoScrollText()
+    {
+        while(myGorectTransform.localPosition.y < boundaryTextEnd) {
+            myGorectTransform.Translate(Vector3.up * speed * Time.deltaTime);
+            yield return null;
+        }
+    }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+}
